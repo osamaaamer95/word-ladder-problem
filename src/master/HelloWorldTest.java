e6375aa3e759d12eea8ba9391e8f88aa1cf2028b
Osama Aamer <osama.aamer95@gmail.com>
2016-10-23 19:09:38 +0500

    Added JUnit

---
+++ src/master/HelloWorldTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package master;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Osama
+ *
+ */
+public class HelloWorldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
