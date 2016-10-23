e0932d80935f5529e7db302ab9c9e6b4bf3dde53
Osama Aamer <osama.aamer95@gmail.com>
2016-10-23 20:16:57 +0500

    Modified dictionary file, added count

--- src/master/HelloWorld.java
+++ src/master/HelloWorld.java
@@ -13,20 +13,25 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-
+		
+		int count = 0;
+		
 		try {
 		JsonParser parser = new JsonParser();
-		JsonElement element = parser.parse(new FileReader("dictionary/testDictionary.json"));
+		JsonElement element = parser.parse(new FileReader("dictionary/dictionary.json"));
 		JsonObject obj = element.getAsJsonObject(); //since you know it's a JsonObject
 		Set<Map.Entry<String, JsonElement>> entries = obj.entrySet();//will return members of your object
 		for (Map.Entry<String, JsonElement> entry: entries) {
 		    System.out.println(entry.getKey());
+		    count++;
 		}
 
 		}
 		catch(FileNotFoundException e) {
 			
 		}
+		
+		System.out.print(count);
 	}
 
 }
